7d2c4d9b798f312ae828c21b08f616ffd56352f8
Stephen Jones <stephen.jones@hireiqinc.com>
2016-02-16 09:07:47 -0500

    basic recipe name search and filtering working.

--- client/containers/RecipeListNav.js
+++ client/containers/RecipeListNav.js
@@ -2,12 +2,12 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 
 import LeftNav from 'material-ui/lib/left-nav';
-import TextField from 'material-ui/lib/text-field';
 import List from 'material-ui/lib/lists/list';
 
 //import LoginForm from 'components/LoginForm';
 import { loadAllRecipes } from 'actions';
 import RecipeListItem from '../components/RecipeListItem';
+import SearchForm from '../containers/SearchForm';
 
 function loadData(props) {
   props.loadRecipes();
@@ -23,7 +23,7 @@ class RecipeListPage extends Component {
 
     return (
       <LeftNav docked={true} style={{top:'65px'}} open={true}>
-        <TextField hintText='search recipe'/>
+        <SearchForm />
         <List>
           {visibleRecipes && visibleRecipes.map((recipeId, index) => <RecipeListItem key={index} selectedRecipeId={selectedRecipeId} recipe={recipes[recipeId]}/>)}
         </List>
@@ -40,9 +40,20 @@ RecipeListPage.propTypes = {
   selectedRecipeId: PropTypes.number
 };
 
+function filteredRecipes(query, recipes) {
+  const lowercaseQuery = query.toLowerCase();
+  if (lowercaseQuery === '') {
+    return Object.keys(recipes);
+  }
+  console.log(recipes);
+  const filteredRecipes = Object.keys(recipes).filter(recipeId => recipes[recipeId].name.toLowerCase().indexOf(lowercaseQuery) > -1);
+  return filteredRecipes;
+}
+
 function mapStateToProps(state, ownProps) {
-  const {entities: {recipes}} = state;
-  const visibleRecipes = Object.keys(recipes);
+  const {form: {search}, entities: {recipes}} = state;
+  // placeholder
+  const visibleRecipes = filteredRecipes(search && search.query.value || '', recipes);
   return {
     recipes,
     selectedRecipeId: Number(ownProps.params.recipeId),
---
+++ client/containers/SearchForm.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react';
+import {reduxForm} from 'redux-form';
+
+import TextField from 'material-ui/lib/text-field';
+
+class SearchForm extends Component {
+  render() {
+    const { fields: { query }, handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <TextField
+          floatingLabelText="Search Recipes"
+          type="text"
+          underlineShow={true}
+          {...query}
+          onBlur={() => {
+            setTimeout(() =>
+              handleSubmit());
+          }}
+        />
+      </form>
+    );
+  }
+}
+
+SearchForm.propTypes = {
+  fields: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+SearchForm = reduxForm({
+  form: 'search',
+  fields: ['query']
+})(SearchForm);
+
+export default SearchForm;
